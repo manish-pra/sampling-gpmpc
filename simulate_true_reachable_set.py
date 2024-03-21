aad7510fb341bd0172200921d092e33825468111
Amon Lahr <amlahr@ethz.ch>
2024-03-21 21:52:46 +0100

    Minor changes

--- simulate_true_reachable_set.py
+++ simulate_true_reachable_set.py
@@ -26,6 +26,7 @@ parser.add_argument("-param", default="params")  # params
 
 parser.add_argument("-env", type=int, default=0)
 parser.add_argument("-i", type=int, default=40)  # initialized at origin
+parser.add_argument("-repeat", type=int, default=1)  # initialized at origin
 args = parser.parse_args()
 
 # 1) Load the config file
